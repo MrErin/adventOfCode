362ded2fe05857a8b979ee5605259ecb9983a351
Erin Meaker <emeaker@essentialfactor.com>
2019-12-03 08:21:31 -0500

    day 3, need to comment

---
+++ 2019/3/1-1_solution.ts
@@ -0,0 +1,95 @@
+import input from './data';
+
+// todo: comment solution to get a better grasp on what's happening.
+
+const getInput = () => {
+  const instructionSets = input.split('\n');
+  return instructionSets;
+};
+
+const intersection = a => {
+  const s = new Set(a[1]);
+  return a[0].filter(x => s.has(x));
+};
+
+const evaluateWireMeasurement = measurementSet => {
+  let pos = [0, 0];
+  let steps = [];
+  let mvts = measurementSet.split(',');
+  mvts.forEach((mvt, index) => {
+    const dir = mvt.slice(0, 1);
+    const dist = Number(mvt.slice(1));
+
+    let i = 0;
+    switch (dir) {
+      case 'R':
+        while (i < dist) {
+          pos[0] += 1;
+          steps.push(pos.toString());
+          i++;
+        }
+        break;
+      case 'L':
+        while (i < dist) {
+          pos[0] -= 1;
+          steps.push(pos.toString());
+          i++;
+        }
+        break;
+      case 'D':
+        while (i < dist) {
+          pos[1] -= 1;
+          steps.push(pos.toString());
+          i++;
+        }
+        break;
+      case 'U':
+        while (i < dist) {
+          pos[1] += 1;
+          steps.push(pos.toString());
+          i++;
+        }
+        break;
+    }
+  });
+  return steps;
+};
+
+const indexOfAll = (arr, val) =>
+  arr.reduce((acc, el, i) => (el === val ? [...acc, i] : acc), []);
+
+const part1 = () => {
+  let positionLists = getInput().map(n => evaluateWireMeasurement(n));
+  let ints = intersection(positionLists);
+  let minDist = null;
+
+  for (let match of ints) {
+    const matches = match.split(',').map(Number);
+    let distance = Math.abs(matches[0]) + Math.abs(matches[1]);
+    if (minDist === null || distance < minDist) {
+      minDist = distance;
+    }
+  }
+  console.log(minDist);
+};
+
+const part2 = () => {
+  let positionLists = getInput().map(n => evaluateWireMeasurement(n));
+  let ints = intersection(positionLists);
+  let minDist = null;
+
+  ints.forEach(match => {
+    const matches = match.split(',').map(Number);
+    let distance =
+      Number(indexOfAll(positionLists[0], match)) +
+      Number(indexOfAll(positionLists[1], match)) +
+      2;
+    if (minDist === null || distance < minDist) {
+      minDist = distance;
+    }
+  });
+  console.log(minDist);
+};
+
+part1();
+part2();
